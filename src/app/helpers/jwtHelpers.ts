3125c15a66b2772ade5276ee02f8f9e2cf6b6fb3
emonhassan83 <emonhasan7650@gmail.com>
2025-11-15 14:51:48 +0600

    zod type error resolved

--- src/app/helpers/jwtHelpers.ts
+++ src/app/helpers/jwtHelpers.ts
@@ -1,26 +1,26 @@
-import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 
 const createToken = (
-  payload: object,
-  secret: Secret,
-  expiresIn: `${number}${"s" | "m" | "h" | "d"}`
+  payload: any,
+  secret: any,
+  expireTime: any
 ): string => {
   return jwt.sign(payload, secret, {
     algorithm: 'HS256',
-    expiresIn,
-  });
+    expiresIn: expireTime as any,
+  } as any);
 };
 
-const verifyToken = (token: string, secret: Secret): JwtPayload => {
-  return jwt.verify(token, secret) as JwtPayload;
+const verifyToken = (token: string, secret: any): JwtPayload => {
+  return jwt.verify(token, secret as any) as JwtPayload;
 };
 
-const createPasswordResetToken = (payload: object) => {
-  return jwt.sign(payload, config.jwt_access_secret as Secret, {
+const createPasswordResetToken = (payload: any) => {
+  return jwt.sign(payload, config.jwt_access_secret as any, {
     algorithm: 'HS256',
-    expiresIn: config.jwt_access_expires_in,  // No need to cast now
-  });
+    expiresIn: config.jwt_access_expires_in as any,
+  } as any);
 };
 
 export const jwtHelpers = {
